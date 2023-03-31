0248be39fff7b73c2b27c3b0981a9b877816d8e7
dirtydenny <dallen7@socal.rr.com>
2023-03-31 09:37:04 -0700

    on to api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,10 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Book.findAll().then((bookData) => {
+    res.json(bookData);
+  });
+
 });
 
 router.get('/:id', (req, res) => {
